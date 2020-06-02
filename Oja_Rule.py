fb909a65ef6d1b82677c4960447c317075eaddd6
Jakob Jordan <jakobjordan@posteo.de>
2020-06-02 18:01:56 +0200

    Refactor and format simulation of Oja's rule

--- Oja_Rule.py
+++ Oja_Rule.py
@@ -1,59 +1,69 @@
 import torch
 import numpy as np
 import matplotlib
-matplotlib.use('TkAgg')
+
+matplotlib.use("TkAgg")
 from matplotlib import pyplot as plt
 from sklearn.decomposition import PCA
 
-# Todo: implement something similar to angle, when dealing with n_dim > 2
-def learn_oja(input_data, initial_weights=None, learning_rate = 0.005):
-    """Implement Oja linearized learning rule
 
+def initialize_weights(initial_weights, n):
+
+    if initial_weights is None:
+        initial_weights = np.random.uniform(-1, 1, size=n)
+
+    # rescale the initial weights to squared sum 1
+    return initial_weights / np.sqrt(np.sum(initial_weights ** 2))
+
+
+def sim(input_data, initial_weights, learning_rate, plasticity_rule):
+    """
     Args:
         input_data (numpy array): An m by n array of datapoints.
             -m: Number of data points
             -n: Number of presynaptic inputs
-        initial_weights (float, optional): initial weight vector
-        learning_rate (float, optional): learning rate, default 0.005
+        initial_weights (float): initial weight vector
+        learning_rate (float): learning rate
+        plasticity_rule (callable): plasticity rule
 
     Returns:
         weights (numpy array): m by n, time course of the weight vector
-        y (numpy array): time course of the output over learning"""
+        y (numpy array): time course of the output over learning
+    """
 
-    m = np.size(input_data,0)
-    n = np.size(input_data,1)
+    m = np.size(input_data, 0)
+    n = np.size(input_data, 1)
 
-    # set initial_weight if none
-    if initial_weights is None:
-        initial_weights = 2* np.random.random_sample(n) - 1 # random values between -1,1
-    initial_weights = initial_weights / np.sqrt(np.sum(np.square(initial_weights))) # rescale the initial weights to squared sum 1
-
-
-    y = np.zeros([m,1])# initialize y
-    weights = np.zeros([m,n])
-    w = initial_weights
-    for i in range(0, len(input_data)):
+    w = initialize_weights(initial_weights, n)
+    y = np.zeros([m, 1])
+    weights = np.zeros([m, n])
+    for i, x in enumerate(input_data):
         weights[i] = w
-        y[i] = np.dot(w, input_data[i])  # output: postsynaptic firing rate of a linear neuron.
-        w += learning_rate * y[i] * (input_data[i] - y[i] * w)
+        y[i] = np.dot(w, x)
+        w = plasticity_rule(w, y[i], x)
+
     return weights, y
 
 
-def learn_normalized_hebbian(input_data, initial_weights=None, learning_rate = 0.005):
-    """ learn with the normalized hebbian rule (Oja-paper equation 2)
+def learn_oja(input_data, initial_weights=None, learning_rate=0.005):
+    """Oja's linearized learning rule (Oja, 1982; eq 3)"""
 
-     Args:
-        input_data (numpy array): An m by n array of datapoints.
-            - m: Number of data points
-            - n: Number of presynaptic inputs
-        initial_weights (float, optional): initial weight vector
-        learning_rate (float, optional): learning rate, default 0.005
+    def plasticity_rule(w, y, x):
+        return w + learning_rate * y * (x - y * w)
 
-    Returns:
-        weights (numpy array): m by n, time course of the weight vector
-        y (numpy array): time course of the output over learning"""
+    return sim(input_data, initial_weights, learning_rate, plasticity_rule)
 
-def create_input_data(num_points, num_dimensions = 2, stds=None):
+
+def learn_normalized_hebbian(input_data, initial_weights=None, learning_rate=0.005):
+    """Normalized Hebbian rule (Oja, 1982; eq 2)"""
+
+    def plasticity_rule(w, y, x):
+        pass  # TODO implement
+
+    return sim(input_data, initial_weights, learning_rate, plasticity_rule)
+
+
+def create_input_data(num_points, num_dimensions=2, stds=None):
     """create an input data set for the learning rules
 
     Args:
@@ -66,24 +76,53 @@ def create_input_data(num_points, num_dimensions = 2, stds=None):
          - m: Number of data points
          - n: Number of presynaptic inputs"""
 
-    if num_points/num_dimensions <= 10:
-        Warning('The ration of num_points to num_dimensions is low, should be at least 10')
+    if num_points / num_dimensions <= 10:
+        Warning(
+            "The ration of num_points to num_dimensions is low, should be at least 10"
+        )
 
     if stds is None:
-        #stds = np.zeros([num_dimensions,1])
         stds = np.random.randn(num_dimensions)
         stds = np.abs(stds)
-        """for idx_dim in range(num_dimensions):
-            stds[idx_dim] = np.random.randn()
-        """
 
-    input_data = np.random.normal(0,stds,[num_points,num_dimensions])
-    return input_data
+    return np.random.normal(0, stds, size=[num_points, num_dimensions])
 
-def plot_oja(input_data, weights, y, do_dataPlot = 'false'):
+
+def compute_first_principle_component(input_data):
+    pca = PCA(n_components=1)
+    pca.fit(input_data)
+    return pca.components_[0]
+
+
+def compute_difference_between_weights_and_first_principle_component(weights, pc1):
+    diff_w_pc1 = np.zeros(len(weights))
+    for idx in range(len(weights)):
+        diff_w_pc1[idx] = np.linalg.norm(weights[idx] - pc1)
+    return diff_w_pc1
+
+
+def compute_angle_between_weights_and_first_principle_component(weights, pc1):
+    assert abs(np.linalg.norm(pc1) - 1.0) < 1e-10
+
+    angles = np.zeros(len(weights))
+    for idx in range(len(weights)):
+        current_normalized_weight = weights[idx] / np.linalg.norm(weights[idx])
+        dot_prod = np.dot(pc1, current_normalized_weight)
+        angles[idx] = np.arccos(dot_prod)
+    return angles
+
+
+def compute_accumulated_variance(y):
+    acc_var_y = np.zeros(len(y))
+    for idx in range(1, len(y)):
+        acc_var_y[idx] = np.var(y[:idx])
+    return acc_var_y
+
+
+def plot_oja(input_data, weights, y, plot_input_data=False):
     """
     Plots the amplitude of the angle between weights and PC1 of the input data,
-    Plots a view of the data and the evolving axis, if do_dataPlot = true and m = 2
+    Plots a view of the data and the evolving axis, if plot_input_data = true and m = 2
 
     Args:
         input_data (numpy.ndarray): m by n data
@@ -92,110 +131,98 @@ def plot_oja(input_data, weights, y, do_dataPlot = 'false'):
 
     """
 
-    # calculate the PC's of the input data (note: this could be placed out of the function also)
-    pca = PCA(n_components=1)
-    pca.fit(input_data)
-    true_pc1 = pca.components_
+    pc1 = compute_first_principle_component(input_data)
+    diff_w_pc1 = compute_difference_between_weights_and_first_principle_component(
+        weights, pc1
+    )
+    angles_w_pc1 = compute_angle_between_weights_and_first_principle_component(
+        weights, pc1
+    )
+    acc_var_y = compute_accumulated_variance(y)
 
-    # plot the summed squared diff (weights - PC1)
-    diff_w_pc1 = np.zeros([np.size(weights,0)])
-    for idx in range(np.size(weights,0)):
-        diff_w_pc1[idx] = np.sqrt(np.sum(np.square(weights[idx,:]-true_pc1)))
-
-    y_min = 0.9*np.min(diff_w_pc1)
-    y_max = 1.1*np.max(diff_w_pc1)
     plt.figure(1)
     plt.plot(diff_w_pc1)
-    plt.ylim([y_min,y_max])
-    plt.xlabel('sample')
-    plt.ylabel('sq diff (w,PC1)')
-    plt.title('Diff w, PC1')
+    plt.ylim([0.0, 5.0])
+    plt.xlabel("sample")
+    plt.ylabel("sq diff (w,PC1)")
+    plt.title("Diff w, PC1")
     plt.show()
 
-    # plot the angle between the weights vector and the PC1
-    angles = np.zeros([np.size(weights,0)])
-    for idx in range(np.size(weights,0)):
-        current_weight = weights[idx] / np.linalg.norm(weights[idx])
-        temp_dot_prod = np.dot(true_pc1, current_weight)
-        angles[idx] = np.arccos(temp_dot_prod)
-    piLine = 3.14*np.ones([np.size(weights,0)])
     plt.figure(2)
-    plt.plot(angles)
-    plt.plot(piLine)
-    plt.ylim([0,4])
-    plt.xlabel('sample')
-    plt.ylabel('angle in rad')
-    plt.title('Angle between w, PC1 in rad')
-    plt.legend(['angles','pi'])
+    plt.plot(angles_w_pc1, color="C0")
+    plt.axhline(np.pi, color="C1")
+    plt.ylim([0, 3.5])
+    plt.xlabel("sample")
+    plt.ylabel("angle in rad")
+    plt.title("Angle between w, PC1 in rad")
+    plt.legend(["angles", "pi"])
     plt.show()
-
-    # plot the (accumulated) variance in y
-    acc_var_y = np.zeros([np.size(y)])
-    for idx in range(1,np.size(y,0)):
-        acc_var_y[idx] = np.var(y[0:idx])
 
     plt.figure(3)
     plt.plot(acc_var_y)
-    plt.ylim([0,1.2*np.max(acc_var_y)])
-    plt.xlabel('sample')
-    plt.ylabel('variance')
-    plt.title('Accumulated variance')
+    plt.ylim([0, 1.2 * np.max(acc_var_y)])
+    plt.xlabel("sample")
+    plt.ylabel("variance")
+    plt.title("Accumulated variance")
     plt.show()
 
-    # plot the data and the final weight vector if desired and n_dim == 2
-    if do_dataPlot:
-        if np.size(input_data,1) != 2:
-            print('Can not do data plot for n != 2')
-            do_dataPlot = 'false'
-        else:
-            plt.scatter(
-                input_data[:, 0],
-                input_data[:, 1],
-                marker=".",
-                facecolor="none",
-                edgecolor="#222222",
-                alpha=.2
-            )
-            plt.xlabel("x1")
-            plt.ylabel("x2")
-
-            # color time and plot with colorbar
-            time = np.arange(len(weights))
-            colors = plt.cm.cool(time / float(len(time)))
-            sm = plt.cm.ScalarMappable(
-                cmap=plt.cm.cool,
-                norm=plt.Normalize(vmin=0, vmax=len(input_data))
-            )
-            sm.set_array(time)
-            cb = plt.colorbar(sm)
-            cb.set_label("Iteration")
-            plt.scatter(
-                weights[:, 0],
-                weights[:, 1],
-                facecolor=colors,
-                edgecolor="none",
-                lw=2
+    if plot_input_data:
+        if np.size(input_data, 1) != 2:
+            raise NotImplementedError(
+                "Can not plot input data for input dimension != 2"
             )
 
-            # ensure rectangular plot
-            x_min = input_data[:, 0].min()
-            x_max = input_data[:, 0].max()
-            y_min = input_data[:, 1].min()
-            y_max = input_data[:, 1].max()
-            lims = [min(x_min, y_min), max(x_max, y_max)]
-            plt.xlim(lims)
-            plt.ylim(lims)
-            plt.show()
+        plt.scatter(
+            input_data[:, 0],
+            input_data[:, 1],
+            marker=".",
+            facecolor="none",
+            edgecolor="#222222",
+            alpha=0.2,
+        )
+        plt.xlabel("x1")
+        plt.ylabel("x2")
 
-def run_oja(num_points, num_dimensions = 2, stds=None, initial_weights=None, learning_rate = 0.005,
-            do_dataPlot = 'false'):
+        # color time and plot with colorbar
+        time = np.arange(len(weights))
+        colors = plt.cm.cool(time / float(len(time)))
+        sm = plt.cm.ScalarMappable(
+            cmap=plt.cm.cool, norm=plt.Normalize(vmin=0, vmax=len(input_data))
+        )
+        sm.set_array(time)
+        cb = plt.colorbar(sm)
+        cb.set_label("Iteration")
+        plt.scatter(
+            weights[:, 0], weights[:, 1], facecolor=colors, edgecolor="none", lw=2
+        )
 
-    input_data = create_input_data(num_points,num_dimensions,stds)
-    [weights, y] = learn_oja(input_data, initial_weights, learning_rate)
-    plot_oja(input_data,weights,y,do_dataPlot)
-    a=1
+        # ensure rectangular plot
+        x_min = input_data[:, 0].min()
+        x_max = input_data[:, 0].max()
+        y_min = input_data[:, 1].min()
+        y_max = input_data[:, 1].max()
+        lims = [min(x_min, y_min), max(x_max, y_max)]
+        plt.xlim(lims)
+        plt.ylim(lims)
+        plt.show()
+
+
+def run_oja(
+    num_points,
+    num_dimensions=2,
+    stds=None,
+    initial_weights=None,
+    learning_rate=0.005,
+    plot_input_data=False,
+):
+
+    input_data = create_input_data(num_points, num_dimensions, stds)
+    weights, y = learn_oja(input_data, initial_weights, learning_rate)
+
+    plot_oja(input_data, weights, y, plot_input_data)
 
 
 if __name__ == "__main__":
-    #run_oja(num_points=1000, do_dataPlot='true')
-    run_oja(num_points=5000, num_dimensions=3)
+    np.random.seed(12)  # always make results reproducible
+
+    run_oja(num_points=5000, num_dimensions=2, plot_input_data=True)
