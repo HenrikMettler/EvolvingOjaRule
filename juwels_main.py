5cc9a9437a0a7a71dfc12602fca6c17a18a07385
HenrikMettler <henrik.mettler@gmail.com>
2020-08-10 10:27:51 +0200

    Change pycharm config so distinction between online and offline is no longer necessary

--- juwels_main.py
+++ juwels_main.py
@@ -15,10 +15,7 @@ from evolution import evolution,  calculate_fitness
 
 if __name__ == "__main__":
 
-    try:
-        seed_offset = int(sys.argv[1])  # For Juwels
-    except IndexError:
-        seed_offset = 1  # For offline debugging
+    seed_offset = int(sys.argv[1])  # For Juwels
 
     with open('params.pickle', 'rb') as f:
         params = pickle.load(f)
